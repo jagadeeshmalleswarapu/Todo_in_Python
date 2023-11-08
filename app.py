45a57e3e064efd7d9218420174561401aef17c9b
Malleswarapu Jagadeesh <MalleswarapuJ@emids.com>
2023-11-08 22:03:39 +0530

    SQL-Online

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from flask_cors import CORS
 from notes import notes
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Notes.db'
+## app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Notes.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://sql12660312:Qbv2YV6Whs@sql12.freesqldatabase.com/sql12660312'
 app.config["JWT_SECRET_KEY"] = "JWT_SECRET_KEY"
 CORS(app=app, origins=['*'])
 
@@ -37,4 +38,4 @@ app.register_blueprint(auth)
 app.register_blueprint(notes)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
