e45ed410ca303e9f92ed8779e39b6322be8b49b7
Jagadeesh Malleswarapu <52293476+jagadeeshmalleswarapu@users.noreply.github.com>
2023-11-09 19:45:51 +0530

    Updated SQL_URI

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ from flask_cors import CORS
 from notes import notes
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Notes.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Notes.db'
+paw = b'YjRBMlcxWTRKVmZwY1psYVRaeXh2SURYTXZwWFp1cGw='
+gethsh = base64.b64decode(paw).decode("utf-8")
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://jagadeeshone:{gethsh}@dpg-cl6dkfiuuipc73cbcl9g-a.oregon-postgres.render.com/testdb_kf2t'
 
 app.config["JWT_SECRET_KEY"] = "JWT_SECRET_KEY"
 CORS(app=app, origins=['*'])
